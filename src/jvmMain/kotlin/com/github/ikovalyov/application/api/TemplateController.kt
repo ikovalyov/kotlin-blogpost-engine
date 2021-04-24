a6e5409a111c6336bbffff3b0a8316902a536feb
Illia Kovalov <illia.kovalov@showpad.com>
2021-04-24 22:34:40 +0200

    template-crud: crud is working

--- src/jvmMain/kotlin/com/github/ikovalyov/application/api/TemplateController.kt
+++ src/jvmMain/kotlin/com/github/ikovalyov/application/api/TemplateController.kt
@@ -10,7 +10,7 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Patch
 import io.micronaut.http.annotation.Post
 
-@Controller("/api/controller")
+@Controller("/api/controller/template")
 class TemplateController(
     private val templateRepository: TemplateRepository
 ) {
