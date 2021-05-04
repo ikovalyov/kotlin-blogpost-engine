9d0d8c679cf2059ac1537153f6c9110c387f9288
Illia Kovalov <illia.kovalov@showpad.com>
2021-05-04 23:10:46 +0200

    js-models: not needed

--- src/commonMain/kotlin/com.github.ikovalyov.model/Person.kt
+++ src/commonMain/kotlin/com.github.ikovalyov.model/Person.kt
@@ -1,6 +1,3 @@
 package com.github.ikovalyov.model
 
-import kotlin.js.ExperimentalJsExport
-import kotlin.js.JsExport
-
-@ExperimentalJsExport @JsExport data class Person(val username: String, val loggedIn: Boolean)
+data class Person(val username: String, val loggedIn: Boolean)
