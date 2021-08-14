51307fcac71e35d28e96337813f770619c64bb8f
Illia Kovalov <illia.kovalov@showpad.com>
2021-08-15 00:19:03 +0200

    109-user-model: spotless

--- src/commonMain/kotlin/com.github.ikovalyov.model/security/Email.kt
+++ src/commonMain/kotlin/com.github.ikovalyov.model/security/Email.kt
@@ -1,5 +1,3 @@
 package com.github.ikovalyov.model.security
 
-import kotlin.jvm.JvmInline
-
-@JvmInline inline class Email(val value: ShortString)
+inline class Email(val value: ShortString)
--- src/commonMain/kotlin/com.github.ikovalyov.model/security/Password.kt
+++ src/commonMain/kotlin/com.github.ikovalyov.model/security/Password.kt
@@ -1,5 +1,3 @@
 package com.github.ikovalyov.model.security
 
-import kotlin.jvm.JvmInline
-
-@JvmInline inline class Password(val value: ShortString)
+inline class Password(val value: ShortString)
