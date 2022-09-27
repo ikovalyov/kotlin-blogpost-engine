47883ebd8247cf1d8cd67f6c7aecd29051c926c2
Illia Kovalov <illia.kovalov@showpad.com>
2022-09-27 09:05:25 +0200

    spotless

--- src/jvmMain/kotlin/com/github/ikovalyov/application/auth/AuthController.kt
+++ src/jvmMain/kotlin/com/github/ikovalyov/application/auth/AuthController.kt
@@ -1,27 +1,19 @@
 package com.github.ikovalyov.application.auth
 
 import com.github.ikovalyov.Api
-import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
-import mu.KotlinLogging
 import java.net.URI
 
 @Controller("/auth")
 class AuthController {
-    private val logger = KotlinLogging.logger { }
-
     @Secured(SecurityRule.IS_AUTHENTICATED)
     @Get("/login")
-    fun login(request: HttpRequest<Any>): HttpResponse<Any> {
+    fun login(): HttpResponse<Any> {
         val location = URI(Api.frontendEndpoint)
-        val response = HttpResponse.redirect<Any>(location)
-        logger.debug {
-            "redirecting to $location"
-        }
-        return response
+        return HttpResponse.redirect(location)
     }
 }
